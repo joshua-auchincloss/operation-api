3331888d21d958935f30e508d84b1832c81630d0
benefactarch <auch.josh@gmail.com>
2025-09-30 18:43:09 -0400

    fix: skip snapshot tests on windows

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -3,6 +3,7 @@ pub use insta::{assert_yaml_snapshot, with_settings};
 #[macro_export]
 macro_rules! insta_test {
     ($f: expr) => {
+        #[cfg(not(target_os = "windows"))]
         $crate::with_settings!({filters => vec![]}, {
             ($f)()
         })
