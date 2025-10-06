7178596e39762de93b5fce3c1f36fbdcb85c242f
benefactarch <auch.josh@gmail.com>
2025-10-06 00:00:00 -0400

    chore: benches

--- parser/benches/parsing.rs
+++ parser/benches/parsing.rs
@@ -1,6 +1,6 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use operation_api_parser::{
-    ast::{self, ty::Type},
+    ast::ty::Type,
     tokens::{Parse, tokenize},
 };
 use std::hint::black_box;
@@ -9,6 +9,41 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("tokenize test message", |b| {
         b.iter(|| tokenize(black_box(include_str!("../samples/message_with_enum.pld"))).unwrap())
     });
+
+    for (t, desc) in &[
+        (
+            "
+            namespace foo;
+
+            #![version(1)]
+
+            struct Abc {
+                a: i32
+            };
+            ",
+            "struct",
+        ),
+        (
+            "
+            namespace test;
+
+            #![version(1)]
+
+            enum IntEnum {
+                A = 1,
+                B = 42,
+            };
+        ",
+            "enum",
+        ),
+    ] {
+        c.bench_function(&format!("ast parse '{desc}'"), |b| {
+            b.iter(|| {
+                let tt = operation_api_parser::ast::AstStream::from_str(black_box(t)).unwrap();
+                std::hint::black_box(tt);
+            })
+        });
+    }
 
     for t in &[
         "i32",
@@ -19,11 +54,10 @@ fn criterion_benchmark(c: &mut Criterion) {
         "i64[][]",
         "i64[]",
     ] {
-        c.bench_function(&format!("parse '{t}'"), |b| {
+        c.bench_function(&format!("type parse '{t}'"), |b| {
+            let tt = tokenize(black_box(t)).unwrap();
             b.iter(|| {
-                let mut tt = tokenize(black_box(t)).unwrap();
-                let t: Type = Type::parse(&mut tt).unwrap();
-                std::hint::black_box(t);
+                let t: Type = Type::parse(&mut std::hint::black_box(tt.clone())).unwrap();
             })
         });
     }
